2f1e9a499ac0bb4b1148249b5e2c995b2c0c2def
Vsevolodovichr <130462736+Vsevolodovichr@users.noreply.github.com>
2023-11-21 20:44:13 +0000

    222

--- porfolio/src/components/Sidebar/index.js
+++ porfolio/src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope, } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faTelegram } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -22,6 +23,32 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a 
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/roman-shkurenko-2084b8b7/">
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a 
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/Vsevolodovichr">
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a 
+                target="_blank"
+                rel="noreferrer"
+                href="https://t.me/Vsevolodovichr">
+                    <FontAwesomeIcon icon={faTelegram} color='#4d4d4e' />
+                </a>
+            </li>
+        </ul>
     </div>
 );
 
